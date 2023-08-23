5261e1d7c306434fe19d1f42c0332061562c972f
T_T <ice257@yandex.ru>
2023-08-23 07:57:21 +0300

    Initial commit

---
+++ src/test/java/com/example/emu4/Application.java
@@ -0,0 +1,11 @@
+package com.example.emu4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emu4ApplicationTests {
+
+    @Test
+
+}
